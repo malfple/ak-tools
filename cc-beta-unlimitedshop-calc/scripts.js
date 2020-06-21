1ea4a0aed94ae84c2af4d6845bfd8645c2214337
malfple <noogai886@yahoo.com>
2020-06-21 23:33:37 +0700

    cc-beta-calc: done

---
+++ cc-beta-unlimitedshop-calc/scripts.js
@@ -0,0 +1,36 @@
+var btCalc = document.getElementById("btCalc");
+btCalc.addEventListener("click", calc);
+
+const a = 90;
+const b = 55;
+
+function calc(){
+	var bounties = document.getElementById("cb").value;
+	var res = document.getElementById("res");
+
+	res.innerHTML = "";
+
+	var n = max_use(bounties);
+	res.innerHTML += "Unused bounties: " + (bounties - n) + "<br>";
+	res.innerHTML += "possible buy plans: <br>";
+
+	gen_all(n, res);
+}
+
+function max_use(bounties){
+	var ret = 0;
+	for(var i = 0; i*a <= bounties; i++){
+		var j = parseInt((bounties - i*a) / b);
+		ret = Math.max(ret, i*a + j*b);
+	}
+	return ret;
+}
+
+function gen_all(usage, res){
+	for(var i = 0; i*a <= usage; i++){
+		var j = parseInt((usage - i*a) / b);
+		if(i*a + j*b != usage) continue;
+
+		res.innerHTML += i + " RMA(s) and " + j + " Polyester(s)<br>";
+	}
+}
