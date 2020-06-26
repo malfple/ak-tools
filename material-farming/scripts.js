de42c48f8250bed62f841db1bbfd4fd8ee78d7bc
malfple <noogai886@yahoo.com>
2020-06-26 11:48:57 +0700

    material-farming: add images

--- material-farming/scripts.js
+++ material-farming/scripts.js
@@ -18,12 +18,13 @@ async function generate(){
 	await fetchPenguinStats();
 	showmsg("Done!");
 
+	showitems();
+
 	btGen.disabled = false;
 	openDefaultTab();
 }
 
 async function fetchjson(link){
-	return [];
 	const response = await fetch(link);
 	const ret = await response.json();
 	return ret;
@@ -47,5 +48,21 @@ async function fetchPenguinStats(){
 		showmsg("Fetching data from penguin-stats: formulas");
 		formula = await fetchjson("https://penguin-stats.io/PenguinStats/api/v2/formula");
 		console.log(formula);
+	}
+}
+
+var test = document.getElementById("test");
+function showitems(){
+	test.innerHTML = "";
+
+	for(let i=0; i<items.length; i++){
+		var c = document.createElement("p");
+		var img = document.createElement("img");
+		img.src = "img/" + items[i].itemId + ".webp";
+		c.append(img);
+		var text = document.createTextNode(items[i].itemId + " " + items[i].itemType + " " + items[i].name_i18n.en);
+		c.appendChild(text);
+
+		test.appendChild(c);
 	}
 }
